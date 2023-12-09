b375e420374567f30f5e71be8d2669465d1d7be9
Artem <artem.egorov.art2006@gmail.com>
2023-12-09 17:10:06 +0300

    Настроил контроллер и изменил конфигурацию
    Теперь БД не пересоздается а обновляется

---
+++ src/main/java/com/artem/carsfactory/Controller/CarController.java
@@ -0,0 +1,96 @@
+package com.artem.carsfactory.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
+
+import com.artem.carsfactory.Domain.Body;
+import com.artem.carsfactory.Domain.Car;
+import com.artem.carsfactory.Domain.Wheel;
+import com.artem.carsfactory.Dto.CarCreateDto;
+import com.artem.carsfactory.Repository.BodyRepository;
+import com.artem.carsfactory.Repository.CarRepository;
+import com.artem.carsfactory.Repository.WheelRepository;
+
+import jakarta.websocket.server.PathParam;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController()
+public class CarController {
+
+    private final BodyRepository bodyRepository;
+    private final WheelRepository wheelRepository;
+    private final CarRepository carRepository;
+
+    public CarController(BodyRepository bodyRepository,
+        WheelRepository wheelRepository,
+        CarRepository carRepository) 
+    {
+        this.bodyRepository = bodyRepository;
+        this.wheelRepository = wheelRepository;
+        this.carRepository = carRepository;
+    }   
+
+    @GetMapping("/bodies")
+    public ResponseEntity<List<Body>> getBodies() {
+        return ResponseEntity.ok(bodyRepository.findAll());
+    }
+
+    @GetMapping("/wheels")
+    public ResponseEntity<List<Wheel>> getWheels() {
+        return ResponseEntity.ok(wheelRepository.findAll());
+    }
+
+    @GetMapping("/car/{id}")
+    public ResponseEntity<Car> getCar(@PathVariable Long id) {
+        Optional<Car> car = carRepository.findById(id);
+
+        if(car.isPresent()) {
+            return ResponseEntity.ok(car.get());
+        }
+
+        return ResponseEntity.badRequest().body(null);
+    }
+
+    @PostMapping("/car")
+    public ResponseEntity<Car> createCar(@RequestBody CarCreateDto model) {
+       Optional<Wheel> wheel = wheelRepository.findById(model.getWheelId());
+       Optional<Body> body = bodyRepository.findById((model.getBodyId()));
+
+       if(wheel.isPresent() && body.isPresent()) {
+           Car car = new Car(wheel.get(), body.get(), model.getName(), model.getCountWheels());
+           Car saveCar = carRepository.save(car);
+           return ResponseEntity.ok(saveCar);
+       }
+        return ResponseEntity.badRequest().body(null);
+    }
+
+    @GetMapping("/cars")
+    public ResponseEntity<List<Car>> getCars(@RequestParam int page, 
+        @RequestParam int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        long totalCount = carRepository.count(); 
+        responseHeaders.set("X-Total-Count",  String.valueOf(totalCount));
+
+        List<Car> cars = carRepository.findAll(pageable).toList();
+
+        return ResponseEntity.ok()
+            .headers(responseHeaders)
+            .body(cars);
+    }   
+}

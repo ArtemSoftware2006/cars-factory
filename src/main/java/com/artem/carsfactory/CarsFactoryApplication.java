72cc2d77ff4ef8282afdb2fea7a82c2dd340a3ca
Artem <artem.egorov.art2006@gmail.com>
2023-12-09 12:36:02 +0300

    initializw

---
+++ src/main/java/com/artem/carsfactory/CarsFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.artem.carsfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artem/carsfactory/CarsFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artem.carsfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
